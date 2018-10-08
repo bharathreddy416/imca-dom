c2efc95314aa37e7d4e3058d34fc07c17a97e8c8
Bharath Reddy Bollampally <bharath.bollampally@techaspect.com>
2018-10-08 11:29:49 +0530

    UI changes

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var IMCA = window.IMCA || {};
+
+IMCA.header = (function (){
+	var self = this;
+	this.bindTopNav = function() {
+		// $('#myTopnav-icon').on('click', function(e) {
+		// 	e.preventDefault();
+		// 	var $myTopnav = $("#myTopnav");
+	 //    	if ($myTopnav.hasClass("topnav")) {
+		//         $myTopnav.addClass("responsive");
+		//     } else {
+		//         $myTopnav.addClass("topnav");
+		//     }
+		// });
+	}
+})();
